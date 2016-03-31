80a958ac465bf44b8541d0a3340d2cb25a7fd5be
Thornfalk <Thorn.falk@gmail.com>
2016-03-31 13:14:37 +0200

    Update! Exercise_13
    The updated version

--- Exercise_13/Program.cs
+++ Exercise_13/Program.cs
@@ -12,44 +12,65 @@ namespace Exercise_13
     {
         static void Main(string[] args)
         {
-            DateTime _dateTimeToday = DateTime.Today;
+            //Gets Todays date
+            DateTime dateTimeToday = DateTime.Today;
 
-            //Gets person name and birth of day 
+
+            //Gets person name and birth
             Console.Write("Name: ");
             string name = Console.ReadLine();
-            Console.Write("Day of birth (YYYY-MM-DD): ");
-            DateTime _birthday = DateTime.Parse(Console.ReadLine());
+            Console.Write("Year of birth: ");
+            int year = int.Parse(Console.ReadLine());
+            Console.Write("Month of birth: ");
+            int month = int.Parse(Console.ReadLine());
+            Console.Write("Day of birth: ");
+            int day = int.Parse(Console.ReadLine());
+
+            DateTime personBirthday = new DateTime(year, month, day);
+
 
             //Calculates persons current age and what he turns to next year
-            DateTime _currentAge = _dateTimeToday.AddYears(-_birthday.Year);
-            DateTime _currentAgePlus1 = _currentAge.AddYears(1);
+            int currentAge = dateTimeToday.Year - personBirthday.Year;
+            DateTime recentBirthday = personBirthday.AddYears(currentAge);
+            if(dateTimeToday < recentBirthday)
+                currentAge--;
+
+
+            //Calculates persons next birthday in days and check if next year is a "leap year"
+            int daysInFebruari = DateTime.DaysInMonth(DateTime.Today.Year+1, 02);
+
+            TimeSpan nextBirthday = recentBirthday - dateTimeToday;
+            int nextBirthdayInDays = 0;
             
-            //Calculates the day person turns 100
-            DateTime _turns100At = _birthday.AddYears(100);
-            TimeSpan _daysTo100 = _turns100At - _dateTimeToday;
-
-            //Calculates persons next birthday in days
-            DateTime _nextBirthday = _birthday.AddYears(_currentAge.Year);
-            TimeSpan _nextBirthdayInDays = _nextBirthday - _dateTimeToday;
-
-            if (_nextBirthdayInDays.Days < 0)
+            if(nextBirthday.Days < 1 && daysInFebruari != 29)
             {
-                Console.WriteLine(@"
-
-Hello, {0}.
-You were {1} years old, but turned {2} {3} days ago. CONGRATULATIONS!
-Your 100th birthday will be on a {4}
-", name, _currentAge.Year, _currentAgePlus1.Year, _nextBirthdayInDays.Days, _turns100At.DayOfWeek);
+                nextBirthdayInDays = nextBirthday.Days + 365;
+            }
+            else if (nextBirthday.Days < 1 && daysInFebruari == 29)
+            {
+                nextBirthdayInDays = nextBirthday.Days + 366;
             }
             else
             {
-                Console.WriteLine(@"
-
-Hello, {0}.
-You are {1} years old, and will turn {2} in {3} days.
-Your 100th birthday will be on a {4}
-", name, _currentAge.Year, _currentAgePlus1.Year, _nextBirthdayInDays.Days, _turns100At.DayOfWeek);
+                nextBirthdayInDays = nextBirthday.Days;
             }
+            
+
+            //Calculates the day person turns 100
+            DateTime turns100At = personBirthday.AddYears(100);
+            TimeSpan daysTo100 = turns100At - dateTimeToday;
+            
+
+            //Printing out the data
+                Console.WriteLine(@"
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+                                                       
+- Hello, {0}.                                           
+- You are {1} years old, and will turn {2} in {3} days. 
+- Your 100th birthday will be on a {4}                  
+                                                       
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+", name, currentAge, currentAge+1, nextBirthdayInDays, turns100At.DayOfWeek);
             Console.ReadKey();
         }
     }
